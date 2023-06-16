2a6cc9cdf23e3f9e8a4e3ec4f6ac76f265f1fd82
emre1n <emre1n@protonmail.com>
2023-06-16 11:14:38 +0300

    add side menu

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+'use client';
+import React, { useState, useCallback } from 'react';
 import Link from 'next/link';
 import { BsList, BsX } from 'react-icons/bs';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen(value => !value);
+  }, []);
+
   return (
     <header className="flex justify-between items-center w-full p-4 bg-base-100">
       <div className="flex justify-center items-center gap-4">
@@ -11,10 +18,35 @@ const Header = () => {
         </Link>
       </div>
       <nav>
-        <BsList
-          className="transition-all hover:cursor-pointer active:scale-90"
-          size="24"
-        />
+        {isMenuOpen ? (
+          <BsX
+            onClick={toggleMenu}
+            className="transition-all hover:cursor-pointer active:scale-90"
+            size="24"
+          />
+        ) : (
+          <BsList
+            onClick={toggleMenu}
+            className="transition-all hover:cursor-pointer active:scale-90"
+            size="24"
+          />
+        )}
+
+        <ul
+          className={`${
+            isMenuOpen ? 'absolute' : 'hidden'
+          } top-16 right-1 p-4 bg-base-100 rounded w-52 flex flex-col items-end gap-4`}
+        >
+          <li>
+            <a>Homepage</a>
+          </li>
+          <li>
+            <a>Portfolio</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
