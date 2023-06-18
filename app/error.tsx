88cfed82c14568edb8220bbb7d05ed187cd73887
emre1n <emre1n@protonmail.com>
2023-06-18 14:35:47 +0300

    add custom error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,11 +1,13 @@
+'use client';
+
 import React from 'react';
 import { TfiFaceSad } from 'react-icons/tfi';
 
-export default function NotFound() {
+export default function Error() {
   return (
     <div className="flex flex-col gap-2 h-full justify-start items-center pt-8">
       <TfiFaceSad size="24" />
-      <h2 className="text-2xl">This post does not exist!</h2>
+      <h2 className="text-2xl">There is an error!</h2>
     </div>
   );
 }
