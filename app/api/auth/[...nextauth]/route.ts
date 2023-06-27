ce30306aba7a9d368dd325eab8b4165d7878c0e5
emre1n <emre1n@protonmail.com>
2023-06-27 11:06:56 +0300

    fix nextauth options export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,7 @@
 import NextAuth, { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-import prisma from '@/libs/prismadb';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   // adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
@@ -20,7 +17,7 @@ export const authOptions: AuthOptions = {
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const res = await fetch('http://localhost:3000/api/login', {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
